81fb951d52b681cae124cc26f5ad5db4786ce7bf
Sjoerd Nijhof <sjoerdnijhof@hotmail.com>
2014-11-28 13:53:52 +0100

    Added a server project

---
+++ Server/Main.cpp
@@ -0,0 +1,10 @@
+// De volgende link is misschien een handige link voor het programmeren van een C++ Server
+// http://beej.us/guide/bgnet/output/html/singlepage/bgnet.html
+
+#include <winsock.h>
+
+int main()
+{
+	// WSADATA wsaData;
+	return 0;
+}

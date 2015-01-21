aea4939c23ae9deccb264225089fba21d3ef0530
Alex Nagelkerke <alex_na@outlook.com>
2015-01-21 20:58:09 +0100

    Building enum

---
+++ MachiavelliServer/Server/BuildingEnum.h
@@ -0,0 +1,24 @@
+//
+//  BuildingEnum.h
+//  Machiavelli
+//
+//  Created by Alex Nagelkerke on 21-01-15.
+//  Copyright (c) 2015 Alex Nagelkerke. All rights reserved.
+//
+
+#ifndef Machiavelli_BuildingEnum_h
+#define Machiavelli_BuildingEnum_h
+
+enum BuildingEnum {
+    GRAVEYARD,
+    HAUNTEDCITY,
+    KEEP,
+    LABORATORY,
+    SMITHY,
+    OBSERVATORY,
+    LIBRARY,
+    MAGICSCHOOL,
+    DRAGONGATE,
+    UNIVERSITY
+};
+#endif
